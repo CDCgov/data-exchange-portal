d6bf66e1b0a9573164357000861ccfd2da9e730a
cfarmer <cfarmer@fearless.tech>
2023-07-24 16:35:56 -0400

    api test code

---
+++ api/src/functions/dexPortalAPI.ts
@@ -0,0 +1,23 @@
+import {
+  app,
+  HttpRequest,
+  HttpResponse,
+  InvocationContext,
+} from "@azure/functions";
+
+export async function dexPortalAPI(
+  context: InvocationContext,
+  request: HttpRequest
+): Promise<HttpResponse> {
+  context.log(`Http function processed request for url "${request.url}"`);
+
+  const name = request.query.get("name") || (await request.text()) || "world";
+
+  return { body: `Hello, ${name}!` };
+}
+
+app.http("oauth", {
+  methods: ["GET", "POST"],
+  authLevel: "anonymous",
+  handler: dexPortalAPI,
+});
