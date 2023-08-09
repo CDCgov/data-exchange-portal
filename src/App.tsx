9462d7df6cf9ff893af09fe3906571607acd1803
cfarmer <cfarmer@fearless.tech>
2023-08-09 14:44:46 -0400

    landing page component and route

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./Dashboard";
+import { Landing } from "./Landing";
+
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Landing />}></Route>
         <Route path="/dashboard" element={<Dashboard />}></Route>
       </Routes>
     </Router>
