59ea5a1c0f9275a995a71f051bf1dca8b6ab79e0
cfarmer <cfarmer@fearless.tech>
2023-08-09 16:39:14 -0400

    main layout for landing page

--- src/Landing.tsx
+++ src/Landing.tsx
@@ -1,3 +1,16 @@
+import styles from "./styles/Landing.module.css";
+import { Button } from "@us-gov-cdc/cdc-react";
+
 export function Landing() {
-  return <h1>Landing</h1>;
+  return (
+    <div className={styles.wrapper}>
+      <section className={styles["logo-section"]}>DEX</section>
+      <section>
+        <h2>Welcome to DEX</h2>
+        <span>The Data Exchange (DEX) Portal</span>
+        <h4>Already have a SAMS login?</h4>
+        <Button ariaLabel="Login with SAMS">Login With SAMS</Button>
+      </section>
+    </div>
+  );
 }
