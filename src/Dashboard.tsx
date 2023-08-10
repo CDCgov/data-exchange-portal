4da5630fa97aca6f22f36a9e4f5ebb57f7fb5c89
Ian Sawyer <49376379+ian-sawyer@users.noreply.github.com>
2023-08-10 13:35:56 -0400

    Added Dividers to login page, added icon to login w/ SAMS button

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -1,7 +1,5 @@
 function Dashboard() {
-  return (
-    <h1>Dashboard</h1>
-  );
+  return <h1>Dashboard</h1>;
 }
 
 export default Dashboard;
--- src/Landing.tsx
+++ src/Landing.tsx
@@ -1,12 +1,18 @@
 import { Logo } from "./components/Logo";
 import styles from "./styles/Landing.module.css";
-import { Button } from "@us-gov-cdc/cdc-react";
+import { Button, Divider } from "@us-gov-cdc/cdc-react";
 
 export function Landing() {
   return (
     <div className={styles.wrapper}>
       <section className={styles["logo-section"]}>
         <Logo name="main" />
+        <Divider
+          height="2"
+          stroke="#C0E9FF"
+          width="11.8rem"
+          className={styles["logo-section-divider"]}
+        />
         <Logo name="cdc" />
       </section>
       <section>
@@ -14,10 +20,31 @@ export function Landing() {
           Welcome to <Logo name="mainSmall" />
         </h2>
         <span>The Data Exchange (DEX) Portal</span>
+        <Divider
+          height="2"
+          stroke="#BDBDBD"
+          className={styles["rightside-divider"]}
+        />
         <h4>Already have a SAMS login?</h4>
-        <Button ariaLabel="Login with SAMS">Login With SAMS</Button>
+        <Button
+          ariaLabel="Login with SAMS"
+          iconPosition="right"
+          iconName="squareArrowUpRight"
+          className={styles["btn-padding-lr"]}
+        >
+          Login With SAMS
+        </Button>
+        <Divider
+          height="2"
+          stroke="#BDBDBD"
+          className={styles["rightside-divider"]}
+        />
         <h4>New to DEX?</h4>
-        <Button ariaLabel="request access" variation="outline">
+        <Button
+          ariaLabel="request access"
+          variation="outline"
+          className={styles["btn-padding-lr"]}
+        >
           Request access
         </Button>
         <Button ariaLabel="learn more" variation="text">
