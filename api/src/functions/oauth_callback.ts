8f89519ed6e2e5dcddfbd726ed3aaf0654cf2720
cfarmer <cfarmer@fearless.tech>
2023-08-10 17:11:56 -0400

    gets the auth token from sams

--- api/src/functions/oauth_callback.ts
+++ api/src/functions/oauth_callback.ts
@@ -2,18 +2,49 @@ import {
   app,
   HttpRequest,
   HttpResponse,
+  HttpResponseBody,
   InvocationContext,
 } from "@azure/functions";
+import axios from "axios";
 
 export async function oauthCallback(
   context: InvocationContext,
   request: HttpRequest
 ): Promise<HttpResponse> {
-  console.log(request.query);
-  console.log(request.body);
+  const authCode = request.query.get("code");
 
-  const authCode = request.query.get("code") || "Did not receive an auth code.";
-  return { body: authCode };
+  if (!authCode) {
+    context.error("Did not receive auth code from auth provider.");
+    return {
+      status: 400,
+      body: "Did not receive auth code from auth provider.",
+    };
+  }
+
+  const samsAuthReqBody = {
+    grant_type: "authorization_code",
+    code: authCode,
+    client_id: process.env["SAMS_CLIENT_ID"],
+    client_secret: process.env["SAMS_CLIENT_SECRET"],
+    redirect_uri: `http://${process.env["WEBSITE_HOSTNAME"]}/api/oauth_callback`,
+  };
+
+  try {
+    const authResponse = await axios({
+      url: `${process.env["SAMS_URL"]}/auth/oauth/v2/token`,
+      method: "post",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      data: samsAuthReqBody,
+    });
+
+    return { body: authResponse.data as HttpResponseBody };
+  } catch (error: unknown) {
+    context.error(error);
+    if (axios.isAxiosError(error)) {
+      return { status: +error.code, body: error.message };
+    }
+    return { status: 500, body: error as HttpResponseBody };
+  }
 }
 
 app.http("oauth_callback", {
