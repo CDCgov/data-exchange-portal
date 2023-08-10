a9a0bc53702d098f354e7c143bce719e950c71df
cfarmer <cfarmer@fearless.tech>
2023-08-10 09:12:49 -0400

    registration content

--- src/Landing.tsx
+++ src/Landing.tsx
@@ -16,6 +16,13 @@ export function Landing() {
         <span>The Data Exchange (DEX) Portal</span>
         <h4>Already have a SAMS login?</h4>
         <Button ariaLabel="Login with SAMS">Login With SAMS</Button>
+        <h4>New to DEX?</h4>
+        <Button ariaLabel="request access" variation="outline">
+          Request access
+        </Button>
+        <Button ariaLabel="learn more" variation="text">
+          Learn more
+        </Button>
       </section>
     </div>
   );
