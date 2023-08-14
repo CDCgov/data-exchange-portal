d81addcf67b69dfb9bb89b25a8729159783b80f5
Ian Sawyer <49376379+ian-sawyer@users.noreply.github.com>
2023-08-14 09:12:49 -0400

    Portal 930/login page (#7)
    * config: add init react router
    
    * landing page component and route
    
    * main layout for landing page
    
    * main logo
    
    * update site title
    
    * logo component
    
    * registration content
    
    * Added Dividers to login page, added icon to login w/ SAMS button
    
    * Updated .gitignore
    
    * Styles for landing page headers, logos
    
    * Updated styles for landing page
    
    * Updated styles utlizing global css import from component library
    
    * Added default widths for divider
    
    * Updated package.json
    
    * Updated landing page styles
    
    ---------
    
    Co-authored-by: Kat Sublett <ksublett@fearless.tech>
    Co-authored-by: cfarmer <cfarmer@fearless.tech>

---
+++ src/Landing.tsx
@@ -0,0 +1,73 @@
+import { Logo } from "./components/Logo";
+import styles from "./styles/Landing.module.css";
+import { Button, Divider } from "@us-gov-cdc/cdc-react";
+import { useAuth } from "react-oidc-context";
+
+export function Landing() {
+  const auth = useAuth();
+
+  return (
+    <div className={styles.wrapper}>
+      <section className={styles["leftside-section"]}>
+        <div className={styles.logos}>
+          <Logo name="main" className={styles["main-logo"]} />
+          <Divider
+            width="118"
+            height="2"
+            stroke="#C0E9FF"
+            className={styles["logo-section-divider"]}
+          />
+          <Logo name="cdc" className={styles["cdc-logo"]} />
+        </div>
+      </section>
+      <section className={styles["rightside-section"]}>
+        <div className={styles["login-content"]}>
+          <div className={styles["login-with-dex"]}>
+            <h2>
+              Welcome to
+              <Logo name="mainSmall" className={styles["welcome-logo"]} />
+            </h2>
+            <span className={styles["welcome-bottom-text"]}>
+              The Data Exchange (DEX) Portal
+            </span>
+            <Divider
+              width="374"
+              height="2"
+              stroke="#BDBDBD"
+              className={styles["rightside-divider"]}
+            />
+            <h4 className={styles["already-sams-user-header"]}>
+              Already have a SAMS login?
+            </h4>
+            <Button
+              ariaLabel="Login with SAMS"
+              iconPosition="right"
+              iconName="squareArrowUpRight"
+              className={styles["login-btn"]}
+              onClick={() => auth.signinRedirect()}>
+              Login With SAMS
+            </Button>
+            <Divider
+              width="374"
+              height="2"
+              stroke="#BDBDBD"
+              className={styles["rightside-divider"]}
+            />
+          </div>
+          <div className={styles["new-to-dex"]}>
+            <h4 className={styles["new-to-dex-header"]}>New to DEX?</h4>
+            <Button
+              ariaLabel="request access"
+              variation="outline"
+              className={styles["request-access-btn"]}>
+              Request access
+            </Button>
+            <Button ariaLabel="learn more" variation="text">
+              Learn more
+            </Button>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
