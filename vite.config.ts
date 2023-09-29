e28799b24fb348ac2ece6560c665210415a54421
cfarmer <cfarmer@fearless.tech>
2023-09-29 10:20:25 -0400

    exclude e2e tests from vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vitest/config";
+import { configDefaults, defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
@@ -7,5 +7,6 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     setupFiles: "./tests/setup.ts",
+    exclude: [...configDefaults.exclude, "./tests/e2e/**"],
   },
 });
