e17ba2da177032f7ff13995ca50bb99957cd6cd5
Chase Farmer <98106992+cfarmer-fearless@users.noreply.github.com>
2023-10-02 16:40:07 -0400

    Portal 1022/playwrite sandbox (#17)
    * feat: e2e tests with playwright
    
    * env variables
    
    * yarn script
    
    * adjust welcome message selector
    
    * exclude e2e tests from vitest
    
    * update yarn lock

---
+++ tests/e2e/landing.spec.ts
@@ -0,0 +1,39 @@
+import { test, expect } from "@playwright/test";
+import { getEnv } from "./utils";
+
+test.describe("Landing Page", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto(getEnv("DEX_URL"));
+  });
+
+  test("has title", async ({ page }) => {
+    await expect(page).toHaveTitle("DEX");
+  });
+
+  test("navigates to SAMS", async ({ page }) => {
+    await page.getByText("Login with SAMS").click();
+
+    await expect(page).toHaveTitle("Secure Access Management Service");
+  });
+
+  test("signs into SAMS", async ({ page }) => {
+    await page.getByText("Login with SAMS").click();
+
+    await page
+      .getByRole("group", { name: "SAMS Credentials" })
+      .locator("#USER")
+      .fill(getEnv("SAMS_USERNAME"));
+    await page
+      .getByRole("group", { name: "SAMS Credentials" })
+      .locator("#PASSWORD")
+      .fill(getEnv("SAMS_PASSWORD"));
+    await page
+      .getByRole("group", { name: "SAMS Credentials" })
+      .getByRole("button")
+      .click();
+
+    await expect(page.getByText(/Welcome/)).toContainText(
+      "chase.farmer@gsa.gov"
+    );
+  });
+});
