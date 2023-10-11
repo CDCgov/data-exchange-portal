7bcaeb28eca0457e1ad4ac1bcabfed5751f6c024
Chase Farmer <98106992+cfarmer-fearless@users.noreply.github.com>
2023-10-11 16:20:39 -0400

    Portal 1036/build number (#23)
    * build number element at bottom of landing page
    
    * WIP unit test
    
    * tests for build number
    
    * workflow step for generating build number
    
    * add app prefix
    
    * remove console log

--- tests/Landing.test.tsx
+++ tests/Landing.test.tsx
@@ -2,6 +2,7 @@ import { render, screen } from "@testing-library/react";
 import { Landing } from "../src/Landing";
 import { createMockedAuthContext, withMockedAuthProvider } from "./helpers";
 import { vi } from "vitest";
+import * as Utils from "../src/utils";
 
 vi.mock("react-oidc-context");
 
@@ -15,5 +16,36 @@ describe("Landing", () => {
     );
 
     expect(screen.getByText("Welcome to")).toBeInTheDocument();
+  });
+
+  it("should show build number when environment variable defined", () => {
+    const testBuildNumber = "1.2.3";
+    const getEnvSpy = vi
+      .spyOn(Utils, "getEnv")
+      .mockReturnValueOnce(testBuildNumber);
+
+    render(
+      withMockedAuthProvider(
+        <Landing />,
+        createMockedAuthContext({ isAuthenticated: false, isLoading: false })
+      )
+    );
+
+    expect(getEnvSpy).toHaveBeenCalled();
+    expect(screen.getByText(`Build 1.2.3`)).toBeInTheDocument();
+  });
+
+  it("should not show build number when environment variable undefined", () => {
+    const getEnvSpy = vi.spyOn(Utils, "getEnv").mockReturnValueOnce("");
+
+    render(
+      withMockedAuthProvider(
+        <Landing />,
+        createMockedAuthContext({ isAuthenticated: false, isLoading: false })
+      )
+    );
+
+    expect(getEnvSpy).toHaveBeenCalled();
+    expect(screen.queryByText("Build")).toBeNull();
   });
 });
