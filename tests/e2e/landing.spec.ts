6fcbc5c653c22e331f0a9bdd5f8c0a9dcf8775a6
cfarmer <cfarmer@fearless.tech>
2023-10-13 15:07:21 -0400

    try wait for url

--- tests/e2e/landing.spec.ts
+++ tests/e2e/landing.spec.ts
@@ -12,14 +12,14 @@ test.describe("Landing Page", () => {
 
   test("navigates to SAMS", async ({ page }) => {
     await page.getByText("Login with SAMS").click();
-    await page.waitForURL("https://auth-stg.cdc.gov/**");
+    await page.waitForURL("**auth-stg.cdc.gov**");
 
     await expect(page).toHaveTitle("Secure Access Management Service");
   });
 
   test("signs into SAMS", async ({ page }) => {
     await page.getByText("Login with SAMS").click();
-    await page.waitForURL("https://auth-stg.cdc.gov/**");
+    await page.waitForURL("**auth-stg.cdc.gov**");
 
     await page
       .getByRole("group", { name: "SAMS Credentials" })
