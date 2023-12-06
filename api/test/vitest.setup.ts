a4c9de860096e8da08f88dda226a7115298eb6fb
Chase Farmer <98106992+cfarmer-fearless@users.noreply.github.com>
2023-12-06 11:19:43 -0500

    PORTAL-1136/Only include src files (#41)
    * only include src files
    
    * test env file

--- api/test/vitest.setup.ts
+++ api/test/vitest.setup.ts
@@ -1,8 +1,8 @@
 import { server } from "./mocks/node";
 import { beforeAll, afterEach, afterAll } from "vitest";
-import config from "../local.settings.json";
+import config from "./test.settings.json";
 
-process.env = Object.assign(process.env, { ...config.Values });
+process.env = Object.assign(process.env, { ...config });
 
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
