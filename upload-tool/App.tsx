c35e3b47b09fc6644efc67e7b48078199bfeee93
Ty Bolt <tybolt@gmail.com>
2024-02-12 10:55:07 -0500

    feat: adds initial create upload page

---
+++ upload-tool/App.tsx
@@ -0,0 +1,24 @@
+import React, { Fragment } from "react";
+
+import "@us-gov-cdc/cdc-react/dist/style.css";
+
+function App() {
+  return (
+    <Fragment>
+      <h1>File Upload</h1>
+      <label htmlFor="portal-upload">Choose a file:</label>
+      <input
+        type="file"
+        id="portal-upload"
+        name="portal-upload"
+        accept=".csv,.hl7,.txt"
+      />
+      <p>
+        Accepted file types include .csv, .hl7, .txt | Limited to 1 file per
+        upload
+      </p>
+    </Fragment>
+  );
+}
+
+export default App;
