076aef2d2e5458b536a61e7f196d3e2933035353
Ty Bolt <tybolt@gmail.com>
2024-02-13 16:49:35 -0500

    feat: add input boxes for environment, data stream, and route

--- upload-tool/App.tsx
+++ upload-tool/App.tsx
@@ -1,26 +1,73 @@
-import React from "react";
-
-import { Button, Divider } from "@us-gov-cdc/cdc-react";
-
-import { Icons } from "@us-gov-cdc/cdc-react-icons";
+import React, { ChangeEvent, useReducer } from "react";
 
 import "@us-gov-cdc/cdc-react/dist/style.css";
+import { Button, Divider, Dropdown } from "@us-gov-cdc/cdc-react";
+import { Icons } from "@us-gov-cdc/cdc-react-icons";
 
 function App() {
   const fileTypes = [".csv", ".hl7", ".txt"];
+
+  const initialState = {
+    fileName: "",
+    environment: "",
+    dataStream: "",
+    route: "",
+  };
+
+  function reducer(state, action) {
+    switch (action.type) {
+      case "updateField":
+        return {
+          ...state,
+          [action.field]: action.payload,
+        };
+      default:
+        throw new Error("Unrecognized action type provided to form reducer");
+    }
+  }
+
+  const [formState, dispatch] = useReducer(reducer, initialState);
 
   const handleFileUpload = () => {
     document?.getElementById("file-uploader")?.click();
   };
 
-  const handleInputChange = (e) => {
-    const file = document.getElementById("file-uploader")?.files[0];
-    const fileName = document.getElementById("file-name");
-    fileName.textContent = file.name;
+  const handleFileNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      dispatch({
+        type: "updateField",
+        field: "fileName",
+        payload: e.target.files[0].name,
+      });
+    }
+  };
+
+  const handleEnvironmentSelect = (item) => {
+    dispatch({
+      type: "updateField",
+      field: "environment",
+      payload: item,
+    });
+  };
+
+  const handleDataStreamSelect = (item) => {
+    dispatch({
+      type: "updateField",
+      field: "dataStream",
+      payload: item,
+    });
+  };
+
+  const handleRouteSelect = (item) => {
+    dispatch({
+      type: "updateField",
+      field: "route",
+      payload: item,
+    });
   };
 
   return (
-    <React.Fragment>
+    <>
       <h1>File Upload</h1>
       <div className="display-flex flex-row flex-justify-start flex-align-center">
         <div className="margin-right-2">
@@ -39,11 +86,11 @@ function App() {
             name="file-uploader"
             accept={fileTypes.toString()}
             multiple
-            onChange={(e) => handleInputChange(e)}
+            onChange={(e) => handleFileNameChange(e)}
           />
         </div>
         <p id="file-name" className="text-italic text-normal">
-          No file chosen
+          {formState.fileName ? formState.fileName : "No file chosen"}
         </p>
       </div>
       <p className="text-italic text-normal">
@@ -52,7 +99,43 @@ function App() {
         per upload
       </p>
       <Divider height={2} stroke="#000" width={1000} />
-    </React.Fragment>
+      <label className="usa-label" htmlFor="environment">
+        Environment
+      </label>
+      <Dropdown
+        id="environment"
+        label="Select an Environment"
+        srText="Select an Environment"
+        items={[]}
+        onSelect={(item) => handleEnvironmentSelect(item)}
+      />
+      <div className="grid-row flex-wrap flex-align-start">
+        <div className="grid-col-3">
+          <label className="usa-label" htmlFor="dataStream">
+            Data Stream
+          </label>
+          <Dropdown
+            id="dataStream"
+            label="Data Stream"
+            srText="Data Stream"
+            items={[]}
+            onSelect={(item) => handleDataStreamSelect(item)}
+          />
+        </div>
+        <div className="grid-col-3">
+          <label className="usa-label" htmlFor="route">
+            Route
+          </label>
+          <Dropdown
+            id="route"
+            label="Select a Route"
+            srText="Select a Route"
+            items={[]}
+            onSelect={(item) => handleRouteSelect(item)}
+          />
+        </div>
+      </div>
+    </>
   );
 }
 
