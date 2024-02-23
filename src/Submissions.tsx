39aba851096ec8f286f1a2920ea83271f967d964
Ty Bolt <tybolt@gmail.com>
2024-02-23 10:51:00 -0500

    minor style tweaks

--- src/Submissions.tsx
+++ src/Submissions.tsx
@@ -1,7 +1,7 @@
 function Submissions() {
   return (
-    <section>
-      <h1 className="font-sans-lg">File Submissions</h1>
+    <section className="bg-grey padding-x-2">
+      <h3 className="padding-top-3">File Submissions</h3>
     </section>
   );
 }
