61656179a8c5a28f2ba3f3ac1a1383d4acd35212
ZacharyRizer <zacharyrizer@gmail.com>
2024-03-11 17:23:49 -0400

    Initial api route setup

--- src/config/api.ts
+++ src/config/api.ts
@@ -4,6 +4,7 @@ const API_URL = getEnv("VITE_API_BASE_URL");
 
 const API_ENDPOINTS = {
   fileSubmissions: `${API_URL}/fileSubmissions`,
+  reportCounts: `${API_URL}/reportCounts`,
   tokenCallback: `${API_URL}/api/token`,
 };
 
