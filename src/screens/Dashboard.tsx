bb3a2623c1d7ed55c89fad3581dc63b3ce71667d
Ty Bolt <tybolt@gmail.com>
2024-03-12 11:16:40 -0400

    wip pie chart

--- src/screens/Dashboard.tsx
+++ src/screens/Dashboard.tsx
@@ -4,6 +4,8 @@ import { useAuth } from "react-oidc-context";
 
 import { Button } from "@us-gov-cdc/cdc-react";
 
+import PieChart from "src/components/PieChart";
+
 function Dashboard() {
   const auth = useAuth();
 
@@ -27,6 +29,40 @@ function Dashboard() {
           Learn more
         </Button>
       </div>
+      <PieChart
+        data={[
+          {
+            id: "hack",
+            label: "hack",
+            value: 274,
+            color: "hsl(88, 70%, 50%)",
+          },
+          {
+            id: "make",
+            label: "make",
+            value: 483,
+            color: "hsl(158, 70%, 50%)",
+          },
+          {
+            id: "elixir",
+            label: "elixir",
+            value: 127,
+            color: "hsl(51, 70%, 50%)",
+          },
+          {
+            id: "go",
+            label: "go",
+            value: 458,
+            color: "hsl(71, 70%, 50%)",
+          },
+          {
+            id: "sass",
+            label: "sass",
+            value: 78,
+            color: "hsl(94, 70%, 50%)",
+          },
+        ]}
+      />
     </section>
   );
 }
