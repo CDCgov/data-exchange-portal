08debf56bd22d03e0d8b9f9b627ebce00c8460e5
Ian Sawyer <49376379+ian-sawyer@users.noreply.github.com>
2024-03-13 14:26:00 -0400

    update: change default dataStreamId

--- src/screens/Submissions.tsx
+++ src/screens/Submissions.tsx
@@ -29,7 +29,7 @@ function Submissions() {
     const fetchCall = async () => {
       const res = await getFileSubmissions(
         auth.user?.access_token || "",
-        "mockDataStream", // TODO: Map to data stream selection
+        "dextesting", // TODO: Map to data stream selection
         new Date().toISOString(), // TODO: Map (date_start) to date selection dropdown
         new Date().toISOString(), // TODO: Map (date_end) to date selection dropdown
         "descending", // TODO: Map to sort_order
