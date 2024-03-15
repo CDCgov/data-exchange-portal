937e365f028bf83271833569cc2c9b44cd8f5f09
Ian Sawyer <49376379+ian-sawyer@users.noreply.github.com>
2024-03-15 10:20:11 -0400

    fix: linting issues

--- src/utils/helperFunctions/date.ts
+++ src/utils/helperFunctions/date.ts
@@ -1,8 +1,8 @@
 export const convertDate = (date: Date) => {
   const convertedDate: string = `${date
     .toISOString()
-    .replace(/\-/g, "")
-    .replace(/\:/g, "")
+    .replace(/-/g, "")
+    .replace(/:/g, "")
     .replace(/\./g, "")
     .slice(0, -4)}Z`;
 
