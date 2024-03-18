4f76b690430de0b63ac9d26152339c825c76bb40
Ty Bolt <tybolt@gmail.com>
2024-03-18 15:14:29 -0400

    lint fix

--- tests/unit/App.test.tsx
+++ tests/unit/App.test.tsx
@@ -1,12 +1,12 @@
-import { render, screen } from "@testing-library/react";
-import App from "src/App";
-import {
-  createMockedAuthContext,
-  withMockedAuthProvider,
-} from "tests/utility/helpers";
-import { vi } from "vitest";
-
-vi.mock("react-oidc-context");
+// import { render, screen } from "@testing-library/react";
+// import App from "src/App";
+// import {
+//   createMockedAuthContext,
+//   withMockedAuthProvider,
+// } from "tests/utility/helpers";
+// import { vi } from "vitest";
+//
+// vi.mock("react-oidc-context");
 
 describe("App", () => {
   it.skip("should render landing page when no user session active", () => {
