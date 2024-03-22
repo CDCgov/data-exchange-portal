bfa10806c9f0cd270a8c3d850a4b089d6b92cfa3
Ian Sawyer <49376379+ian-sawyer@users.noreply.github.com>
2024-03-22 13:46:57 -0400

    update: adjust for optional params /reportCounts endpoint

--- api/src/main/kotlin/com/dexportalapi/DeXPortalAPI/web/MainController.kt
+++ api/src/main/kotlin/com/dexportalapi/DeXPortalAPI/web/MainController.kt
@@ -26,6 +26,12 @@ class MainController() {
     private val webClient = WebClient.builder().build()
 
     @CrossOrigin
+    @GetMapping("/")
+    fun index(): String {
+        return "Status: OK"
+    }
+
+    @CrossOrigin
     @GetMapping("/health")
     fun health(): String {
         return "Status: OK"
@@ -69,22 +75,31 @@ class MainController() {
     suspend fun getReportCountsRequest(
             @RequestParam("data_stream_id") dataStreamId: String,
             @RequestParam("data_stream_route") dataStreamRoute: String,
-            @RequestParam("date_start") dateStart: String,
-            @RequestParam("date_end") dateEnd: String,
-            @RequestParam("ext_event") extEvent: String,
+            @RequestParam("date_start") dateStart: String? = "",
+            @RequestParam("date_end") dateEnd: String? = "",
+            @RequestParam("ext_event") extEvent: String? = "",
             @RequestHeader("Authorization") authToken: String
     ): String {
         val psAPIUrl: String = System.getenv("PS_API_URL").toString()
+        var uri: String = psAPIUrl + "/api/report/counts/submissions/summary?data_stream_id=" +
+                                        dataStreamId + "&data_stream_route=" + dataStreamRoute
+
+        if (dateStart != null && dateStart.length > 0) {
+            uri = uri + "&data_start=" + dateStart
+        }
+        if (dateEnd != null && dateEnd.length > 0) {
+            uri = uri + "&data_end=" + dateEnd
+        }
+        if (extEvent != null && extEvent.length > 0) {
+            uri = uri + "&ext_event=" + extEvent
+        }
+
+        print(uri)
 
         val response =
                 webClient
                         .get()
-                        .uri(
-                                psAPIUrl +
-                                        "/api/report/counts?data_stream_id=" +
-                                        dataStreamId + "&data_stream_route=" + dataStreamRoute + 
-                                        "&date_start=" + dateStart + "&date_end=" + dateEnd + "&ext_event=" + extEvent
-                        )
+                        .uri(uri)
                         .header("Authorization", authToken)
                         .retrieve()
                         .onStatus({ responseStatus ->
@@ -123,37 +138,41 @@ class MainController() {
 
     @CrossOrigin
     @PostMapping("/api/token")
-    suspend fun postAuthTokenRequest(@RequestParam("code") authCode: String): String {
-        val samsUrl: String = System.getenv("SAMS_URL").toString()
-        val clientId: String = System.getenv("SAMS_CLIENT_ID").toString()
-        val clientSecret: String = System.getenv("SAMS_CLIENT_SECRET").toString()
-        val redirectURI: String = System.getenv("SAMS_REDIRECT_URL").toString()
+    suspend fun postAuthTokenRequest(@RequestParam("code") authCode: String? = ""): String {
+        if (authCode != null && authCode.length > 0) {
+            val samsUrl: String = System.getenv("SAMS_URL").toString()
+            val clientId: String = System.getenv("SAMS_CLIENT_ID").toString()
+            val clientSecret: String = System.getenv("SAMS_CLIENT_SECRET").toString()
+            val redirectURI: String = System.getenv("SAMS_REDIRECT_URL").toString()
 
-        val grantType: String = "authorization_code"
-        val scope: String = "dex:status"
+            val grantType: String = "authorization_code"
+            val scope: String = "dex:status"
 
-        val response =
-                webClient
-                        .post()
-                        .uri(samsUrl + "/auth/oauth/v2/token")
-                        .contentType(APPLICATION_FORM_URLENCODED)
-                        .body(
-                                BodyInserters.fromFormData("grant_type", grantType)
-                                        .with("code", authCode)
-                                        .with("client_id", clientId)
-                                        .with("client_secret", clientSecret)
-                                        .with("redirect_uri", redirectURI)
-                                        .with("scope", scope)
-                        )
-                        .retrieve()
-                        .onStatus({ responseStatus ->
-                            responseStatus == HttpStatus.INTERNAL_SERVER_ERROR
-                        }) { throw ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR) }
-                        .onStatus({ responseStatus -> responseStatus == HttpStatus.BAD_REQUEST }) {
-                            throw ResponseStatusException(HttpStatus.BAD_REQUEST)
-                        }
-                        .awaitBody<String>()
+            val response =
+                    webClient
+                            .post()
+                            .uri(samsUrl + "/auth/oauth/v2/token")
+                            .contentType(APPLICATION_FORM_URLENCODED)
+                            .body(
+                                    BodyInserters.fromFormData("grant_type", grantType)
+                                            .with("code", authCode)
+                                            .with("client_id", clientId)
+                                            .with("client_secret", clientSecret)
+                                            .with("redirect_uri", redirectURI)
+                                            .with("scope", scope)
+                            )
+                            .retrieve()
+                            .onStatus({ responseStatus ->
+                                responseStatus == HttpStatus.INTERNAL_SERVER_ERROR
+                            }) { throw ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR) }
+                            .onStatus({ responseStatus -> responseStatus == HttpStatus.BAD_REQUEST }) {
+                                throw ResponseStatusException(HttpStatus.BAD_REQUEST)
+                            }
+                            .awaitBody<String>()
 
-        return response
+            return response
+        } else {
+            return "No Auth Code Present"
+        }
     }
 }
