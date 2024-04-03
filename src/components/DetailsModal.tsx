406e14cb83d3cd142fcc6f18abec7c3992cab7d8
ZacharyRizer <zacharyrizer@gmail.com>
2024-04-03 11:15:06 -0400

    Add specific logic file status type in the details modal

--- src/components/DetailsModal.tsx
+++ src/components/DetailsModal.tsx
@@ -6,7 +6,9 @@ import {
   ModalBody,
   ModalFooter,
   Pill,
+  ProgressTracker,
 } from "@us-gov-cdc/cdc-react";
+import { Icons } from "@us-gov-cdc/cdc-react-icons";
 import { IFileSubmission } from "@types";
 import getStatusDisplayValuesById, {
   StatusDisplayValues,
@@ -27,30 +29,64 @@ function DetailsModal({
     submission.status
   );
 
+  const getContent = () => {
+    if (submission.status == "failed") {
+      /*
+      TODO: parse file details for failed alert data
+      Use a useEffect to make the submissionDetails call to continually update
+      */
+      return (
+        <Alert heading="Failed Metadata" type="error">
+          2 Errors were detected
+          <p className="border-1px radius-md border-secondary-light margin-y-2 padding-105">
+            Missing required metadata field, 'meta_field1.'
+          </p>
+          <p className="border-1px radius-md border-secondary-light margin-y-2 padding-105">
+            Metadata field, 'meta_field2,' is set to 'value3' and does not
+            contain one of the allowed values: ['value1', 'value2']
+          </p>
+        </Alert>
+      );
+    }
+
+    if (submission.status == "completed") {
+      return <ProgressTracker isComplete />;
+    }
+
+    if (submission.status == "processing") {
+      /*
+      TODO: parse file details for stage, current upload amount, total upload amount, etc.
+      Use a useEffect to make the submissionDetails call to continually update
+      */
+      return (
+        <ProgressTracker
+          label="Stage: Uploading"
+          currentAmount={55}
+          totalAmount={256}
+        />
+      );
+    }
+  };
+
   return (
     <Modal
       isOpen={isModalOpen}
       modalTitle="Submission Details"
       onClose={handleModalClose}>
       <ModalBody>
-        <h2 className="font-sans-md text-bold">{submission.filename}</h2>
-        <span className="padding-top-2 padding-bottom-2">
+        <span className="padding-bottom-3">
+          {/* TODO: update cdc-react to allow className on <Pill> so we can avoid this extra span */}
           <Pill
             label={displayValues.label}
             color={displayValues.pillColor}></Pill>
         </span>
-        {/* Todo: update cdc-react to allow className on <Pill> so we can avoid this extra span */}
-        <Alert heading="Failed Metadata" type="error">
-          2 Errors were detected
-          {/* Todo: rework this since the alert body is a <p> and you can't have a <div> nested inside a <p> */}
-          <div className="border-1px radius-md border-secondary-light margin-y-2 padding-105">
-            Missing required metadata field, 'meta_field1.'
-          </div>
-          <div className="border-1px radius-md border-secondary-light margin-y-2 padding-105">
-            Metadata field, 'meta_field2,' is set to 'value3' and does not
-            contain one of the allowed values: ['value1', 'value2']
-          </div>
-        </Alert>
+        <div className="grid-row flex-row flex-align-center padding-bottom-3">
+          <Icons.PaperLines />
+          <h2 className="font-sans-md text-bold padding-left-1">
+            {submission.filename}
+          </h2>
+        </div>
+        {getContent()}
         <Divider className="margin-y-2" height={4} stroke="#E0E0E0" />
         <div className="grid-row margin-y-1">
           <div className="grid-col-4">
