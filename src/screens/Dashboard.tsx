17406708326466f67e2301deacf2fe095990d908
ZacharyRizer <zacharyrizer@gmail.com>
2024-05-07 19:14:04 -0400

    Added routes, datastream, and timeframe to dashboard

--- src/screens/Dashboard.tsx
+++ src/screens/Dashboard.tsx
@@ -1,5 +1,7 @@
 import { useAuth } from "react-oidc-context";
 
+import { Dropdown } from "@us-gov-cdc/cdc-react";
+import { Icons } from "@us-gov-cdc/cdc-react-icons";
 import PieChart from "src/components/PieChart";
 import StatusBoxes from "src/components/StatusBoxes";
 
@@ -10,7 +12,8 @@ import getReportCounts, {
   ReportCounts,
 } from "src/utils/api/reportCounts";
 
-import convertDate from "src/utils/helperFunctions/date";
+import convertDate, { getPastDate } from "src/utils/helperFunctions/date";
+import timeframes, { Timeframe } from "src/types/timeframes";
 import { useFeatureFlag } from "src/utils/hooks/useFeatureFlag";
 
 function Dashboard() {
@@ -19,6 +22,10 @@ function Dashboard() {
     useState<ReportCounts>(defaultReportCounts);
 
   const { enabled: enableSuperUser } = useFeatureFlag("EnableSuperUser");
+
+  const [dataStream, setDataStream] = useState("aims-celr");
+  const [dataRoute, setDataRoute] = useState("csv");
+  const [timeframe, setTimeframe] = useState<Timeframe>(Timeframe.Last30Days);
 
   useEffect(() => {
     console.log(
@@ -30,9 +37,9 @@ function Dashboard() {
     const fetchCall = async () => {
       const res = await getReportCounts(
         auth.user?.access_token || "",
-        "temp_data_stream_id",
-        "temp_data_stream_route",
-        "",
+        dataStream,
+        dataRoute,
+        getPastDate(timeframe),
         convertDate(new Date())
       );
 
@@ -47,11 +54,41 @@ function Dashboard() {
       }
     };
     fetchCall();
-  }, [auth]);
+  }, [auth, dataStream, dataRoute, timeframe]);
+
+  const handleTimeframe = (time: string) => {
+    const timeframe = time as Timeframe;
+    setTimeframe(timeframe);
+  };
 
   return (
     <section className="main_content padding-x-2">
       <h1 className="cdc-page-header padding-y-3 margin-0">Dashboard</h1>
+      <div className="padding-bottom-2 display-flex flex-row flex-justify">
+        <div className="display-flex flex-row cdc-submissions-page--filters">
+          <Dropdown
+            className="padding-right-2"
+            items={["aims-celr", "daart"]}
+            label="Data Stream"
+            onSelect={setDataStream}
+            srText="Data Stream"
+          />
+          <Dropdown
+            className="padding-right-2"
+            items={["csv", "hl7"]}
+            label="Route"
+            onSelect={setDataRoute}
+            srText="Data Route"
+          />
+          <Dropdown
+            items={timeframes}
+            label="Timeframe"
+            labelIcon={<Icons.Calendar />}
+            onSelect={handleTimeframe}
+            srText="Timeframe"
+          />
+        </div>
+      </div>
       <StatusBoxes data={countsData} />
       <PieChart data={countsData} />
     </section>
