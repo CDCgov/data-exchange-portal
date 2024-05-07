8d7e5d584e00eae277bd5fb54b27f3a9b8aac792
ZacharyRizer <zacharyrizer@gmail.com>
2024-05-07 19:04:46 -0400

    Added route and pastDay conversions

--- src/utils/helperFunctions/date.ts
+++ src/utils/helperFunctions/date.ts
@@ -1,4 +1,6 @@
-const convertDate = (date: Date) => {
+import { Timeframe } from "src/types/timeframes";
+
+const convertDate = (date: Date): string => {
   const convertedDate: string = `${date
     .toISOString()
     .replace(/-/g, "")
@@ -7,6 +9,23 @@ const convertDate = (date: Date) => {
     .slice(0, -4)}Z`;
 
   return convertedDate;
+};
+
+export const getPastDate = (daysBefore: Timeframe): string => {
+  const today = new Date();
+
+  switch (daysBefore) {
+    case Timeframe.Last30Days:
+      return convertDate(new Date(new Date().setDate(today.getDate() - 30)));
+    case Timeframe.Last15Days:
+      return convertDate(new Date(new Date().setDate(today.getDate() - 15)));
+    case Timeframe.Last7Days:
+      return convertDate(new Date(new Date().setDate(today.getDate() - 7)));
+    case Timeframe.LastDay:
+      return convertDate(new Date(new Date().setDate(today.getDate() - 1)));
+    default:
+      return convertDate(today);
+  }
 };
 
 export default convertDate;
