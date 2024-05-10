6e86a03404ca505a5348d2a20ce2b30c22769280
Ty Bolt <tybolt@gmail.com>
2024-05-10 10:44:54 -0400

    removes comments based on feedback

--- api/src/main/kotlin/com/dexportalapi/DeXPortalAPI/utils/DateUtils.kt
+++ api/src/main/kotlin/com/dexportalapi/DeXPortalAPI/utils/DateUtils.kt
@@ -5,13 +5,10 @@ import java.time.format.DateTimeFormatter
 
 fun processDateString(inputDate: String): String? {
     try {
-        // Parse the date string to a LocalDateTime object
         val parsedDate = LocalDateTime.parse(inputDate, DateTimeFormatter.ISO_DATE_TIME)
 
-        // Convert to ISO date string without milliseconds
         val isoDateString = parsedDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'"))
 
-        // Remove ":","-","." characters
         val cleanedDateString = isoDateString.replace(":", "").replace("-", "").replace(".", "")
 
         return cleanedDateString
