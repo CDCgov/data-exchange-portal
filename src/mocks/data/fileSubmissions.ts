30958037725fbe9a57638caf7d1e664152398a2a
ZacharyRizer <zacharyrizer@gmail.com>
2024-05-28 10:01:48 -0400

    Submission details rework

--- src/mocks/data/fileSubmissions.ts
+++ src/mocks/data/fileSubmissions.ts
@@ -15,19 +15,35 @@ const weightedStatuses = [
   "Uploading",
 ];
 
+const getIssues = (status: string, route: string): string[] => {
+  if (status.toLowerCase().includes("failed")) {
+    console.log(status);
+    return route == "csv"
+      ? [
+          "Missing required metadata field, 'meta_field1'.",
+          "Metadata field, 'meta_field2' is set to 'value3' and does not contain one of the allowed values: [ 'value1', value2' ]",
+        ]
+      : ["Hl7 Validation Error -- See validation report"];
+  }
+
+  return [];
+};
+
 const generateFileSubmission = (
   dataStream: string,
   route: string
 ): FileSubmission => {
+  const status = faker.helpers.arrayElement(weightedStatuses);
   const submission: FileSubmission = {
     upload_id: faker.string.uuid(),
     filename: faker.system.commonFileName(route),
-    status: faker.helpers.arrayElement(weightedStatuses),
+    status: status,
     timestamp: faker.date.recent({ days: 40 }).toISOString(),
     metadata: {
       data_stream_id: dataStream,
       data_stream_route: route,
     },
+    issues: getIssues(status, route),
   };
   return submission;
 };
--- src/mocks/data/submissionDetails.ts
+++ src/mocks/data/submissionDetails.ts
@@ -1,95 +1,25 @@
 import { faker } from "@faker-js/faker";
-import {
-  Report,
-  ReportError,
-  SubmissionDetails,
-} from "src/utils/api/submissionDetails";
+import { Report, SubmissionDetails } from "src/utils/api/submissionDetails";
 import { FileSubmission } from "src/utils/api/fileSubmissions";
 import mockSubmissions from "src/mocks/data/fileSubmissions";
 
-const generateError = (stage: string, action: string): ReportError => {
+const generateReport = (submission: FileSubmission, stage: string): Report => {
   return {
-    type: "validation",
-    stage: stage,
-    action: action,
-    level: "Error",
-    message: "There was a problem. Here's an actionable message",
+    report_id: faker.string.uuid(),
+    stage_name: stage,
+    timestamp: submission.timestamp,
+    content: {
+      schema_name: "mock_schema",
+      schema_version: "0.0.1",
+    },
   };
 };
 
-const getErrors = (
-  submission: FileSubmission,
-  stage: string,
-  action: string
-): ReportError[] => {
-  if (submission.status == "failed") {
-    const numOfErrors = faker.number.int({ min: 1, max: 3 });
-    const errors: ReportError[] = [];
-    for (let i = 0; i < numOfErrors; i++) {
-      errors.push(generateError(stage, action));
-    }
-    return errors;
-  }
-  return [];
-};
-
-const generateReport = (
-  submission: FileSubmission,
-  stage: string,
-  action: string
-): Report => {
-  const getType = () => {
-    switch (submission.status) {
-      case "failed":
-        return "failure";
-      case "processing":
-        return "pending";
-      case "completed":
-        return "success";
-      default:
-        return "success";
-    }
-  };
-  const getMessage = () => {
-    switch (submission.status) {
-      case "failure":
-        return "There was an issue with this report.";
-      case "processing":
-        return "The report is pending.";
-      case "completed":
-        return "Successfully completed this stage.";
-      default:
-        return "";
-    }
-  };
-
-  return {
-    stage: stage,
-    action: action,
-    schema_name: "0.0.1",
-    version: "version 1",
-    data_stream_id: submission.metadata?.data_stream_id,
-    data_stream_route: submission.metadata?.data_stream_route,
-    jurisdiction: faker.location.state({ abbreviated: true }),
-    transport_id: submission.upload_id,
-    parent_id: faker.string.uuid(),
-    message_id: faker.string.uuid(),
-    type: getType(),
-    timestamp_complete: submission.timestamp,
-    message: getMessage(),
-    errors: getErrors(submission, stage, action),
-  };
-};
-
-const getReports = (
-  submission: FileSubmission,
-  stage: string,
-  action: string
-): Report[] => {
+const getReports = (submission: FileSubmission, stage: string): Report[] => {
   const numOfReports = faker.number.int({ min: 1, max: 3 });
   const reports: Report[] = [];
   for (let i = 0; i < numOfReports; i++) {
-    reports.push(generateReport(submission, stage, action));
+    reports.push(generateReport(submission, stage));
   }
   return reports;
 };
@@ -99,18 +29,12 @@ const generateSubmissionDetails = (
 ): SubmissionDetails => {
   const route = submission.metadata?.data_stream_route;
   const stage = route == "hl7" ? "dex-hl7" : "dex-upload";
-  const action = stage == "dex-hl7" ? "validation" : "metadata-validation";
 
   const details: SubmissionDetails = {
-    status: submission.status,
-    current_stage: stage,
-    current_action: action,
-    file_name: submission.filename,
-    transport_id: submission.upload_id,
-    timestamp: submission.timestamp,
+    upload_id: submission.upload_id,
     data_stream_id: submission.metadata?.data_stream_id,
     data_stream_route: submission.metadata?.data_stream_route,
-    reports: getReports(submission, stage, action),
+    reports: getReports(submission, stage),
   };
   return details;
 };
@@ -128,9 +52,7 @@ const buildMockDetails = (): SubmissionDetails[] => {
 export const mockDetails = buildMockDetails();
 
 const getMockDetails = (upload_id: string): SubmissionDetails | undefined => {
-  return mockDetails.find(
-    (el: SubmissionDetails) => el.transport_id == upload_id
-  );
+  return mockDetails.find((el: SubmissionDetails) => el.upload_id == upload_id);
 };
 
 export default getMockDetails;
