603abbbd60b504c4be280bbb793100870f4d7fd0
Ty Bolt <tybolt@gmail.com>
2024-05-30 15:49:28 -0400

    add basic health check (#148)

--- api/src/main/kotlin/dexportal/routes/Routing.kt
+++ api/src/main/kotlin/dexportal/routes/Routing.kt
@@ -1,5 +1,6 @@
 package dexportal.routes
 
+import dexportal.config.ConfigLoader
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -7,14 +8,26 @@ import dexportal.routes.psApi.*
 import dexportal.routes.mms.*
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
 import io.ktor.server.auth.*
 
 fun Application.configureRouting() {
     val client = HttpClient(CIO)
 
+    val mmsUrl = ConfigLoader.getMmsApiEndpoint()
+
     routing {
         get("/") {
             call.respondText("Status: OK")
+        }
+        // Todo: Revisit this once we've debugged the 403 issue
+        get("/health") {
+            val response: HttpResponse = client.get("$mmsUrl/health")
+            val responseBody: String = response.bodyAsText()
+            call.application.log.info("Health Check Response: $response")
+            call.application.log.info("Health Check ResponseBody: $responseBody")
+            call.respondText("MMS /health Response: $responseBody")
         }
         authRoutes(client)
         authenticate("auth-bearer") {
