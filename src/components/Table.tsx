a0c9c0f7779dd81d857e1e433094e340fb665060
ZacharyRizer <zacharyrizer@gmail.com>
2024-06-05 15:51:01 -0400

    Removing if statement

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -21,9 +21,6 @@ function PortalTable<TData>({ table }: PortalTableProps<TData>) {
     if (field === "status" || field === "details") {
       return "sm";
     }
-    if (field === "timestamp") {
-      return "md";
-    }
     return "md";
   };
 
