caa39bcc0dde112ca6e22da8180207bf278fae71
Ty Bolt <tybolt@gmail.com>
2024-06-18 11:39:37 -0400

    split out manifest definitions

--- src/screens/UploadFiles.tsx
+++ src/screens/UploadFiles.tsx
@@ -1,5 +1,6 @@
 import { ChangeEvent, useReducer, useState, useEffect } from "react";
 
+import ManifestDefinitions from "src/components/ManifestDefs";
 import { Button } from "../../src/components/Button";
 import { Alert } from "@us-gov-cdc/cdc-react";
 
@@ -189,60 +190,7 @@ function UploadFiles() {
             <h2 className="font-sans-lg text-normal margin-bottom-2">
               Help: Submission Manifest Fields
             </h2>
-            <dl>
-              <dt className="font-mono-md padding-bottom-1">
-                <strong>data_producer_id</strong>
-              </dt>
-              <dd>
-                An alphabetical code that identifies the public health authority
-                that supplies the data. In cases where the data producer is the
-                same as the sender, the value should match the sender_id. In
-                cases where this is not provided or known, use a value of
-                "null."
-              </dd>
-              <hr className="margin-y-2 border-1px border-base-lighter" />
-              <dt className="font-mono-md padding-bottom-1">
-                <strong>data_stream_id</strong>
-              </dt>
-              <dd>
-                An alphabetical code that identifies the CDC program receiving
-                the data. This code is provided during onboarding.
-              </dd>
-              <hr className="margin-y-2 border-1px border-base-lighter" />
-              <dt className="font-mono-md padding-bottom-1">
-                <strong>data_stream_route</strong>
-              </dt>
-              <dd>
-                The name of the folder destination for the data file. Typically
-                this will match the submission file format.
-              </dd>
-              <hr className="margin-y-2 border-1px border-base-lighter" />
-              <dt className="font-mono-md padding-bottom-1">
-                <strong>jurisdiction</strong>
-              </dt>
-              <dd>
-                An alphabetical code that identifies the city, county, state,
-                tribe, or territory of the sender. This code is provided during
-                onboarding.
-              </dd>
-              <hr className="margin-y-2 border-1px border-base-lighter" />
-              <dt className="font-mono-md padding-bottom-1">
-                <strong>sender_id</strong>
-              </dt>
-              <dd>
-                An alphabetical code that identifies the sender, machine, or
-                intermediary organization that's sending data. This code is
-                provided during onboarding.
-              </dd>
-              <hr className="margin-y-2 border-1px border-base-lighter" />
-              <dt className="font-mono-md padding-bottom-1">
-                <strong>version</strong>
-              </dt>
-              <dd>
-                The version of the metadata being sent. In all new cases, this
-                field value will be "2.0"
-              </dd>
-            </dl>
+            <ManifestDefinitions />
           </div>
         </div>
       </section>
