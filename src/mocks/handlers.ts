d36191231d57ede8967c80bdebfd2e8ad1ab4524
ZacharyRizer <zacharyrizer@gmail.com>
2024-06-20 16:06:35 -0400

    Disable mocking

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,5 +1,4 @@
-import { bypassedHandlers } from "src/mocks/handlers/bypass";
 import { mmsHandlers } from "src/mocks/handlers/mmsHandlers";
 import { psApiHandlers } from "src/mocks/handlers/psApiHandlers";
 
-export const handlers = [...bypassedHandlers, ...mmsHandlers, ...psApiHandlers];
+export const handlers = [...mmsHandlers, ...psApiHandlers];
--- src/mocks/handlers/bypass.ts
+++
@@ -1,8 +0,0 @@
-import { http, passthrough } from "msw";
-import API_ENDPOINTS from "src/config/api";
-
-export const bypassedHandlers = [
-  http.all(API_ENDPOINTS.upload, () => {
-    return passthrough();
-  }),
-];
