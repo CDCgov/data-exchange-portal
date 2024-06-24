27cbe3eac189e3775cf6fd0c72ac3c78f0a37c43
ZacharyRizer <zacharyrizer@gmail.com>
2024-06-24 11:28:05 -0400

    Helperfunction for date check

--- tests/unit/utils/helperFunctions/date.test.ts
+++ tests/unit/utils/helperFunctions/date.test.ts
@@ -1,5 +1,5 @@
 import { vi } from "vitest";
-import { getPastDate } from "src/utils/helperFunctions/date";
+import { getPastDate, isValidIsoString } from "src/utils/helperFunctions/date";
 import { Timeframe } from "src/types/timeframes";
 
 beforeAll(() => {
@@ -31,5 +31,32 @@ describe("Get Past Date - Timeframes", () => {
     const pastDate = getPastDate(Timeframe.LastDay);
     const pastDateValue = "2019-12-31T00:00:00.000Z";
     expect(pastDate).toStrictEqual(pastDateValue);
+  });
+});
+
+describe("isValidIsoString", () => {
+  it("should return true for a valid ISO string", () => {
+    const validISODate = "2023-06-24T10:30:00Z";
+    expect(isValidIsoString(validISODate)).toBe(true);
+  });
+
+  it("should return false for an invalid ISO string", () => {
+    const invalidISODate = "2023-13-40T10:30:00Z";
+    expect(isValidIsoString(invalidISODate)).toBe(false);
+  });
+
+  it("should return false for an empty string", () => {
+    const emptyString = "";
+    expect(isValidIsoString(emptyString)).toBe(false);
+  });
+
+  it("should return false for a null value", () => {
+    const nullValue = null as unknown as string;
+    expect(isValidIsoString(nullValue)).toBe(false);
+  });
+
+  it("should return false for a non-ISO date string", () => {
+    const nonISODateString = "June 24, 2023";
+    expect(isValidIsoString(nonISODateString)).toBe(false);
   });
 });
