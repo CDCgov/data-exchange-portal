0e94e554f897b6b1bbf4926fa8b113d7a6d79c65
ZacharyRizer <zacharyrizer@gmail.com>
2024-07-15 09:38:09 -0400

    Removed API_BASE_URL

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,24 +1,23 @@
 import { getEnv } from "src/utils/helperFunctions/env";
 
-const API_URL = getEnv("VITE_API_BASE_URL");
 const UPLOAD_URL = getEnv("VITE_UPLOAD_API_ENDPOINT");
 const MMS_PREFIX = "mms";
 const PS_API_PREFIX = "ps-api";
 
 const API_ENDPOINTS = {
   // mms routes
-  currentUserDatastreamRoutes: `${API_URL}/${MMS_PREFIX}/current-user/datastreams-with-routes`,
-  dataStreams: `${API_URL}/${MMS_PREFIX}/datastreams`,
-  entities: `${API_URL}/${MMS_PREFIX}/entities`,
-  groups: `${API_URL}/${MMS_PREFIX}/groups`,
-  identities: `${API_URL}/${MMS_PREFIX}/identities`,
-  dataStreamRouteGroups: `${API_URL}/${MMS_PREFIX}/datastream-routes-groups`,
+  currentUserDatastreamRoutes: `api/${MMS_PREFIX}/current-user/datastreams-with-routes`,
+  dataStreams: `api/${MMS_PREFIX}/datastreams`,
+  entities: `api/${MMS_PREFIX}/entities`,
+  groups: `api/${MMS_PREFIX}/groups`,
+  identities: `api/${MMS_PREFIX}/identities`,
+  dataStreamRouteGroups: `api/${MMS_PREFIX}/datastream-routes-groups`,
   // ps api routes
-  fileSubmissions: `${API_URL}/${PS_API_PREFIX}/file-submissions`,
-  reportCounts: `${API_URL}/${PS_API_PREFIX}/report-counts`,
-  submissionDetails: `${API_URL}/${PS_API_PREFIX}/submission-details`,
+  fileSubmissions: `api/${PS_API_PREFIX}/file-submissions`,
+  reportCounts: `api/${PS_API_PREFIX}/report-counts`,
+  submissionDetails: `api/${PS_API_PREFIX}/submission-details`,
   // auth
-  tokenCallback: `${API_URL}/api/token`,
+  tokenCallback: `api/token`,
   // upload
   upload: UPLOAD_URL,
 };
