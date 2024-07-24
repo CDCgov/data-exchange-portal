8835c40dcb9703c23b3296623ade1c22ccdfef0d
ZacharyRizer <zacharyrizer@gmail.com>
2024-07-24 10:55:31 -0400

    Add tests for write only helper functions

--- tests/unit/utils/helperFunctions/metadataFilters.test.ts
+++ tests/unit/utils/helperFunctions/metadataFilters.test.ts
@@ -1,7 +1,9 @@
 import { describe, it, expect } from "vitest";
 import {
   getDataStreamIds,
+  getDataStreamIdsWriteOnly,
   getDataRoutes,
+  getDataRoutesWriteOnly,
 } from "src/utils/helperFunctions/metadataFilters";
 import { DataStreamWithRoutes } from "src/utils/api/dataStreams";
 
@@ -9,13 +11,15 @@ const mockData: DataStreamWithRoutes[] = [
   {
     datastream: { id: 1, name: "Stream1" },
     routes: [
-      { id: 1, dataStreamID: 1, name: "Route1" },
-      { id: 2, dataStreamID: 1, name: "Route2" },
+      { id: 1, dataStreamID: 1, name: "Route1", writePermissions: true },
+      { id: 2, dataStreamID: 1, name: "Route2", writePermissions: false },
     ],
   },
   {
     datastream: { id: 2, name: "Stream2" },
-    routes: [{ id: 3, dataStreamID: 2, name: "Route3" }],
+    routes: [
+      { id: 3, dataStreamID: 2, name: "Route3", writePermissions: false },
+    ],
   },
 ];
 
@@ -34,5 +38,18 @@ describe("Helper Functions", () => {
 
     const result3 = getDataRoutes(mockData, "NonExistentStream");
     expect(result3).toEqual([]);
+  });
+
+  it("should return only datastream names with routes that are write only permissions", () => {
+    const result = getDataStreamIdsWriteOnly(mockData);
+    expect(result).toEqual(["Stream1"]);
+  });
+
+  it("should return routes for a given datastream name that have write permissions", () => {
+    const result = getDataRoutesWriteOnly(mockData, "Stream1");
+    expect(result).toEqual(["Route1"]);
+
+    const result2 = getDataRoutesWriteOnly(mockData, "Stream2");
+    expect(result2).toEqual([]);
   });
 });
