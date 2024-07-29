90f46df30a756b3e61a9a53b5cf0fc1239db6511
ZacharyRizer <zacharyrizer@gmail.com>
2024-07-29 14:42:56 -0400

    Tests for submissions query

--- api/src/test/kotlin/dexportal/routes/psApi/SubmissionsTest.kt
+++ api/src/test/kotlin/dexportal/routes/psApi/SubmissionsTest.kt
@@ -2,7 +2,7 @@ package dexportal.routes.psApi
 
 import com.apollographql.apollo3.ApolloClient
 import com.apollographql.apollo3.api.ApolloResponse
-import com.dexportal.CountsQuery
+import com.dexportal.SubmissionsQuery
 import dexportal.utils.testModule
 import dexportal.utils.toJsonString
 import io.ktor.client.request.*
@@ -16,37 +16,61 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
-class CountsRouteTest {
+class SubmissionsRouteTest {
     private val mockApolloClient = mockk<ApolloClient>()
 
     @Test
-    fun testCountsRouteSuccess() = testApplication {
+    fun testSubmissionsRouteSuccess() = testApplication {
         val mockRequestUuid = UUID.fromString("123e4567-e89b-12d3-a456-426614174000")
-        val mockData = CountsQuery.Data(
-            processingCounts = CountsQuery.ProcessingCounts(
-                totalCounts = 100, statusCounts = CountsQuery.StatusCounts(
-                    failed = CountsQuery.Failed(counts = 10),
-                    uploaded = CountsQuery.Uploaded(counts = 80),
-                    uploading = CountsQuery.Uploading(counts = 10)
+        val mockData = SubmissionsQuery.Data(
+            uploads = SubmissionsQuery.Uploads(
+                summary = SubmissionsQuery.Summary(
+                    numberOfPages = 1,
+                    pageNumber = 1,
+                    pageSize = 1,
+                    totalItems = 10
+                ),
+                items = listOf(
+                    SubmissionsQuery.Item(
+                        bytesUploaded = 50,
+                        fileName = "Mock File",
+                        fileSizeBytes = 50,
+                        issues = listOf(),
+                        metadata = null,
+                        percentComplete = 50.0,
+                        status = "Mock Status",
+                        timeUploadingSec = 10.0,
+                        timestamp = "mock time",
+                        uploadId = "abc123"
+                    )
                 )
             ),
         )
 
         val mockResponse = ApolloResponse.Builder(
-            operation = CountsQuery("dataStream", "dataRoute", "20230728T154500Z", "20230729T154500Z"),
+            operation = SubmissionsQuery(
+                "dataStream",
+                "dataRoute",
+                "20230728T154500Z",
+                "20230729T154500Z",
+                10,
+                1,
+                "",
+                ""
+            ),
             data = mockData,
             requestUuid = mockRequestUuid
         ).build()
 
         coEvery {
-            mockApolloClient.query(any<CountsQuery>()).addHttpHeader("Authorization", any()).execute()
+            mockApolloClient.query(any<SubmissionsQuery>()).addHttpHeader("Authorization", any()).execute()
         } returns mockResponse
 
         application {
             testModule(mockApolloClient)
         }
 
-        client.get("/report-counts?data_stream_id=dataStream&data_stream_route=dataRoute&date_start=2023-07-28T15:45:00&date_end=2023-07-29T15:45:00") {
+        client.get("/file-submissions?data_stream_id=dataStream&data_stream_route=dataRoute&date_start=2023-07-28T15:45:00&date_end=2023-07-29T15:45:00") {
             header(HttpHeaders.Authorization, "Bearer token")
         }.apply {
             assertEquals(HttpStatusCode.OK, status)
@@ -55,12 +79,12 @@ class CountsRouteTest {
     }
 
     @Test
-    fun testCountsRouteMissingDataStream() = testApplication {
+    fun testSubmissionsRouteMissingDataStream() = testApplication {
         application {
             testModule(mockApolloClient)
         }
 
-        client.get("/report-counts?data_stream_route=dataRoute&date_start=2023-07-28T15:45:00&date_end=2023-07-29T15:45:00") {
+        client.get("/file-submissions?data_stream_route=dataRoute&date_start=2023-07-28T15:45:00&date_end=2023-07-29T15:45:00") {
             header(HttpHeaders.Authorization, "Bearer token")
         }.apply {
             assertEquals(HttpStatusCode.BadRequest, status)
@@ -69,12 +93,12 @@ class CountsRouteTest {
     }
 
     @Test
-    fun testCountsRouteMissingDateStart() = testApplication {
+    fun testSubmissionsRouteMissingDateStart() = testApplication {
         application {
             testModule(mockApolloClient)
         }
 
-        client.get("/report-counts?data_stream_id=dataStream&data_stream_route=dataRoute&date_start=&date_end=2023-07-29T15:45:00") {
+        client.get("/file-submissions?data_stream_id=dataStream&data_stream_route=dataRoute&date_start=&date_end=2023-07-29T15:45:00") {
             header(HttpHeaders.Authorization, "Bearer token")
         }.apply {
             assertEquals(HttpStatusCode.BadRequest, status)
@@ -83,12 +107,12 @@ class CountsRouteTest {
     }
 
     @Test
-    fun testCountsRouteMissingDateEnd() = testApplication {
+    fun testSubmissionsRouteMissingDateEnd() = testApplication {
         application {
             testModule(mockApolloClient)
         }
 
-        client.get("/report-counts?data_stream_id=dataStream&data_stream_route=dataRoute&date_start=2023-07-29T15:45:00") {
+        client.get("/file-submissions?data_stream_id=dataStream&data_stream_route=dataRoute&date_start=2023-07-29T15:45:00") {
             header(HttpHeaders.Authorization, "Bearer token")
         }.apply {
             assertEquals(HttpStatusCode.BadRequest, status)
@@ -97,12 +121,12 @@ class CountsRouteTest {
     }
 
     @Test
-    fun testCountsRouteMissingAuthToken() = testApplication {
+    fun testSubmissionsRouteMissingAuthToken() = testApplication {
         application {
             testModule(mockApolloClient)
         }
 
-        client.get("/report-counts?data_stream_id=dataStream&data_stream_route=dataRoute&date_start=2023-07-28T15:45:00&date_end=2023-07-29T15:45:00") {
+        client.get("/file-submissions?data_stream_id=dataStream&data_stream_route=dataRoute&date_start=2023-07-28T15:45:00&date_end=2023-07-29T15:45:00") {
             header(HttpHeaders.Authorization, "")
         }.apply {
             assertEquals(HttpStatusCode.BadRequest, status)
@@ -111,14 +135,14 @@ class CountsRouteTest {
     }
 
     @Test
-    fun testCountsRouteInvalidDate() = testApplication {
+    fun testSubmissionsRouteInvalidDate() = testApplication {
         application {
             testModule(mockApolloClient)
         }
 
         val inputDate = "invalid-date"
 
-        client.get("/report-counts?data_stream_id=dataStream&data_stream_route=dataRoute&date_start=$inputDate&date_end=2023-07-29T15:45:00") {
+        client.get("/file-submissions?data_stream_id=dataStream&data_stream_route=dataRoute&date_start=$inputDate&date_end=2023-07-29T15:45:00") {
             header(HttpHeaders.Authorization, "Bearer token")
         }.apply {
             assertEquals(HttpStatusCode.BadRequest, status)
